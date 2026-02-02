1dc695e5b0e64a4b5f0f8a42ab1f49f60d804f76
KormanCarter <kormancarter@gmail.com>
2026-02-02 08:57:45 -0700

    first commit

---
+++ server/server.js
@@ -0,0 +1,18 @@
+// server/server.js
+const path = require("path");
+const express = require("express");
+
+
+console.log("Hello")
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+app.use(express.static(path.resolve(__dirname, "../client/dist")));
+
+// Handle GET requests to /api route
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from server!" });
+});
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
